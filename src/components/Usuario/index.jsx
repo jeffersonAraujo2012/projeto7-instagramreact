cc4e4fc78d7d7e1fb1bbcb5eb3821edae63caad1
Jefferson Araújo <jeffersonc201212@gmail.com>
2022-11-20 17:05:04 -0300

    Add: funcionalidade de trocar a imagem de perfil do usuário.

--- src/components/Usuario/index.jsx
+++ src/components/Usuario/index.jsx
@@ -2,24 +2,35 @@ import { useState } from "react";
 
 export default function Usuario() {
   const [nomeUsuario, setNomeUsuario] = useState("Catana");
+  const [imgUsuario, setImgUsuario] = useState("./images/catanacomics 1.png");
 
-  function handlingEditarNomeUsuario() {
+  function handlerEditarNomeUsuario() {
     const novoNome = prompt("Qual novo nome de usuário?");
     if (novoNome) setNomeUsuario(novoNome);
     else alert("Alteração não realizada! O nome não pode ser vazio.");
   }
 
+  function handlerEditarImgUsuario() {
+    const novaImg = prompt("Qual link da nova imagem?");
+    if (novaImg) setImgUsuario(novaImg);
+    else alert("Alteração não realizada! O link não pode ser vazio.");
+  }
+
   return (
     <div className="perfil perfil--my">
       <a href="#">
-        <img src="./images/catanacomics 1.png" alt="Minha imagem de perfil" />
+        <img
+          src={imgUsuario}
+          alt="Minha imagem de perfil"
+          onClick={handlerEditarImgUsuario}
+        />
       </a>
       <div>
         <a href="#">catanacomics</a>
         <p>
           {nomeUsuario}&nbsp;
           <ion-icon
-            onClick={handlingEditarNomeUsuario}
+            onClick={handlerEditarNomeUsuario}
             name="pencil-outline"
           ></ion-icon>
         </p>
